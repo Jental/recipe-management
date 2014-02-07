496a2697a431d0d0689ea8bd153ecd02fe83726c
Jental <jentalmeister@gmail.com>
2014-02-07 11:05:54 +0400

    Prettier png2mht

--- recipe_png_to_webpage/png2mht.py
+++ recipe_png_to_webpage/png2mht.py
@@ -3,30 +3,37 @@ import chilkat
 import urllib.request
 import urllib.parse
 
+INPUT_DIR = "/home/jental/Cook/"
+OUTPUT_DIR = "/home/jental/tmp/Cook/"
+REQUEST_URL = "https://duckduckgo.com/html/?%s"
+
+if (not os.path.isfile(OUTPUT_DIR)) and (not os.path.isdir(OUTPUT_DIR)):
+  os.mkdir(OUTPUT_DIR)
+
 mht = chilkat.CkMht()
 success = mht.UnlockComponent("Anything for 30-day trial")
+if not success:
+  exit()
 
-proxy_support = urllib.request.ProxyHandler({"https" : "127.0.0.1:8118"})
+proxy_support = urllib.request.ProxyHandler({"https" : "127.0.0.1:8118"}) # Privoxy
 opener = urllib.request.build_opener(proxy_support)
 
-for filename in os.listdir("/home/jental/Cook/"):
+for filename in os.listdir(INPUT_DIR):
   if filename.endswith(".png"):
     query = filename[:-18]
-    newFileName = "/home/jental/dev/Cook/" + query + ".mht"
+    newFileName = OUTPUT_DIR + "/" + query + ".mht"
     if not os.path.isfile(newFileName):
       params = urllib.parse.urlencode({"q" : "!ducky " + query})
       try:
-        handler = opener.open("https://duckduckgo.com/html/?%s" % params)
+        handler = opener.open(REQUEST_URL % params)
       except:
-        print("Failed to execute a request: " + "https://duckduckgo.com/html/?%s" % params)
+        print("Failed to execute a request: " + REQUEST_URL % params)
         continue
       foundUrl = handler.geturl()
       handler.close()
       print(foundUrl)
       # print(foundUrl)
-      # mhtRes = mht.GetAndSaveMHT(foundUrl, u"/home/jental/dev/Cook/" + query + u".mht")
       mhtRes = mht.getMHT(foundUrl)
-      # if mhtRes != True:
       if mhtRes == None:
         try:
           print("Failed to download: ", foundUrl, mht.lastErrorText)
